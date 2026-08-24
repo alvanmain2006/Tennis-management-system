b377eae05aeb64cb6dc9a5d2300a569868bc0df7
Tri Vo <trivo006wa@gmail.com>
2026-08-23 22:22:21 -0700

    adding all till now might need to refactor

---
+++ src/repositories/playerRepositories.js
@@ -0,0 +1,13 @@
+const pool = require("../config/db");
+
+class PlayerRepository {
+    async getAllPlayers() {
+        const result = await pool.query(
+                "SELECT * FROM players;"
+            );
+
+            return result.rows;
+    }
+}
+
+module.exports = new PlayerRepository();
--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
+require("dotenv").config();
+const pool = require("../config/db")
 const app = require("./app");
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+
+async function startServer() {
+    try {
+        const result = await pool.query("SELECT NOW();")
+        console.log(result.rows);
+        console.log("DB is connected to Postgrest")
+
+        app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+
+    } catch(error) {
+        console.error("❌ Failed to connect to PostgreSQL");
+        console.error(error);
+
+        process.exit(1);
+    }
+
+}
+
+startServer()
